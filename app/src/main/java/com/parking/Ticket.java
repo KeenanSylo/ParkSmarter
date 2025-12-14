3989ecadfbe06cd04cde8d108854b06a4649a07a
Keenan Arsianto <sylokzk@gmail.com>
2025-12-15 00:31:15 +0100

    GREEN: implement ID for ticket

--- app/src/main/java/com/parking/Ticket.java
+++ app/src/main/java/com/parking/Ticket.java
@@ -1,10 +1,12 @@
 package com.parking;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 public class Ticket {
     
     private final LocalDateTime entryTime;
+    private final UUID id;
 
     // Constructor
     public Ticket(LocalDateTime entryTime) {
@@ -12,10 +14,16 @@ public class Ticket {
             throw new IllegalArgumentException("Entry time cannot be null");
         }
         this.entryTime = entryTime;
+        this.id = UUID.randomUUID();
     }
 
     // Getter for entryTime
     public LocalDateTime getEntryTime() {
         return entryTime;
+    }
+
+    // Getter for id
+    public UUID getId() {
+        return id;
     }
 }
